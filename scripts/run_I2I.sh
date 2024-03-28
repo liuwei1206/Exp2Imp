52d3eb48592e3651626e7dce4e2361a851dba7f1
liuwei1206 <willie1206@163.com>
2024-03-28 13:25:54 +0100

    fine

--- scripts/run_I2I.sh
+++ scripts/run_I2I.sh
@@ -1,49 +1,57 @@
-# <<"COMMENT"
-python3 base.py --do_train \
-                --dataset="pdtb2"  \
-                --label_file="labels_1.txt" \
-                --relation_type="implicit" \
-                --train_batch_size=16 \
-                --learning_rate=1e-5 \
-                --num_train_epochs=10
-# COMMENT
-
-## for pdtb
+## for pdtb 2.0
 <<"COMMENT"
-for dataset in pdtb2 pdtb3
-do
-    for rel_type in implicit
-    do
-        for level in 1 2
-        do
-            python3 base.py --do_train \
-                            --dataset=${dataset}  \
-                            --label_file="labels_${level}.txt" \
-                            --relation_type=${rel_type} \
-                            --train_batch_size=16 \
-                            --learning_rate=1e-5 \
-                            --num_train_epochs=10
-        done
-    done
-done
+python3 analyze.py --do_train \
+                   --dataset="pdtb2"  \
+                   --label_file="labels_1.txt" \
+                   --relation_type="implicit" \
+                   --train_batch_size=16 \
+                   --learning_rate=1e-5 \
+                   --num_train_epochs=10
+COMMENT
+<<"COMMENT"
+
+<<"COMMENT"
+python3 analyze.py --do_train \
+                   --dataset="pdtb2"  \
+                   --label_file="labels_2.txt" \
+                   --relation_type="implicit" \
+                   --train_batch_size=16 \
+                   --learning_rate=1e-5 \
+                   --num_train_epochs=10
 COMMENT
 
-## for gum dataset
+
+## for pdtb 3.0
 <<"COMMENT"
-for dataset in gum7
-do
-    for rel_type in implicit
-    do
-        for level in 1
-        do
-            python3 base.py --do_train \
-                            --dataset=${dataset}  \
-                            --label_file="labels_${level}.txt" \
-                            --relation_type=${rel_type} \
-                            --train_batch_size=16 \
-                            --learning_rate=1e-5 \
-                            --num_train_epochs=10
-        done
-    done
-done
+python3 analyze.py --do_train \
+                   --dataset="pdtb3"  \
+                   --label_file="labels_1.txt" \
+                   --relation_type="implicit" \
+                   --train_batch_size=16 \
+                   --learning_rate=1e-5 \
+                   --num_train_epochs=10
 COMMENT
+<<"COMMENT"
+
+<<"COMMENT"
+python3 analyze.py --do_train \
+                   --dataset="pdtb3"  \
+                   --label_file="labels_2.txt" \
+                   --relation_type="implicit" \
+                   --train_batch_size=16 \
+                   --learning_rate=1e-5 \
+                   --num_train_epochs=10
+COMMENT
+
+
+## for gum7
+<<"COMMENT"
+python3 analyze.py --do_train \
+                   --dataset="gum7"  \
+                   --label_file="labels_1.txt" \
+                   --relation_type="implicit" \
+                   --train_batch_size=16 \
+                   --learning_rate=1e-5 \
+                   --num_train_epochs=15
+COMMENT
+<<"COMMENT"
